c7a7012d47a860c52303e1334bd7370df7d91bc4
Евгений Клименко <deeech88@gmail.com>
2016-12-30 23:50:14 +0300

    add: pages

--- src/js/entry.jsx
+++ src/js/entry.jsx
@@ -3,6 +3,9 @@ import Hello from './components/Hello'
 import Clicker from './components/Clicker'
 import testModule from './moduletest'
 
+// Pages
+import Index from 'pages/Index'
+
 let i = 0
 console.log('Hello! I am an entry point!')
 testModule()
---
+++ src/js/pages/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Index extends React.component {
+  render() {
+    return (
+      <div>
+        <h1>Index Page<h1>
+      </div>
+    )
+  }
+}
